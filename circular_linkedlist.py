0809f46974112d8e0236506fee25be1b3bda9188
jpnovkiddevop <jpnovkid@gmail.com>
2024-01-21 20:09:10 +0300

    created delete at the beginning func for circular linked list and added self.tail variable

--- circular_linkedlist.py
+++ circular_linkedlist.py
@@ -6,6 +6,7 @@ class Node:
 class CircularLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
     
     def append(self):
         choices = 1
@@ -13,13 +14,26 @@ class CircularLinkedList:
             new_node = Node(input("Enter the element to be added at the end of the list "))
             if not self.head:
                 self.head = new_node
-                new_node.next = self.head
+                self.tail = new_node
+                new_node.next = self.head             
             current = self.head
             while current.next != self.head:
                 current = current.next
-            current.next = new_node
             new_node.next = self.head
-            choices = int(input("\nDo you want to add more elements? Enter \'1' or any other no to quit "))
+            self.tail = new_node
+            current.next = new_node
+            choices = int(input("\nDo you want to add more elements? Enter \'1' or any other number to quit "))
+            
+    def delete_at_beginning(self):
+        if not self.head:
+            print("you cant delete from an empty list")
+            return
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        self.head = self.head.next
+        self.tail.next = self.head
+        
     
     def display(self):
         if not self.head:
@@ -33,4 +47,8 @@ class CircularLinkedList:
 
 cll = CircularLinkedList()
 cll.append()
+print("list contains: ")
+cll.display()
+cll.delete_at_beginning()
+print("list after deleting at the beginning: ")
 cll.display()
