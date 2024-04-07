eb84093815308c36690ba509161f446033a45466
jp <jpnovkid@gmail.com>
2024-04-07 21:57:47 +0300

    added the inorder, preorder and postorder traversal functions in tree file

--- tree.py
+++ tree.py
@@ -2,20 +2,20 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.left = None
-        self.right = None
+        self.right = None        
 
-    def insert(self, data):
+    def insert_data(self, data):
         if self.data:
             if data < self.data:
                 if self.left is None:
                     self.left = Node(data)
                 else:
-                    self.left.insert(data)
-            elif data > self.data:
+                    self.left.insert_data(data)
+            if data > self.data:
                 if self.right is None:
                     self.right = Node(data)
                 else:
-                    self.right.insert(data)
+                    self.right.insert_data(data)
         else:
             self.data = Node(data)
 
@@ -26,12 +26,33 @@ class Node:
         if self.right:
             self.right.display()
 
+    def inorder_traversal(self, root):
+        if root:
+            self.inorder_traversal(root.left)
+            print(root.data)
+            self.inorder_traversal(root.right)       
 
-tree = Node(3)
-tree.insert(5)
-tree.insert(7)
-tree.insert(2)
-tree.insert(0)
-tree.insert(8)
-print("Tree elements: ")
-tree.display() # output: 0,2,3,5,7,8
+    def preorder_traversal(self, root):
+        if root:
+            print(root.data)
+            self.preorder_traversal(root.left)
+            self.preorder_traversal(root.right)
+
+    def postorder_traversal(self, root):
+        if root:
+            self.postorder_traversal(root.left)
+            self.postorder_traversal(root.right)
+            print(root.data)
+
+
+root = Node(3)
+root.insert_data(5)
+root.right.insert_data(13)
+root.insert_data(6)
+root.display()
+print("tree elements in inorder traversal: ")
+root.inorder_traversal(root)
+print("tree elements in preorder: ")
+root.preorder_traversal(root)
+print("tree elements after postorder traversal: ")
+root.postorder_traversal(root)
